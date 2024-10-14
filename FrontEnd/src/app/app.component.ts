4f69eb12d6869ccaa663e7c552926e73b026f86f
balamurugan1999v <31644214+balamurugan1999v@users.noreply.github.com>
2024-10-14 10:35:25 +0530

    Form example

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -7,4 +7,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   balu = 'Naanthan';
+  name:any;
+
+  process(){
+    console.log("Coming here");
+  }
+
+  mailId:any;
+  number:any;
+  address:any;
+  jsonObj:any;
+  jsonData:any;
+  nameResponse:any;
+
+  submit(){
+    this.jsonObj={ 
+      name: this.name,
+      mailId: this.mailId,
+      contactNumber: this.number,
+      address: this.address
+    };
+    console.log(this.jsonObj);
+    this.jsonData = JSON.stringify(this.jsonObj);
+    console.log(this.jsonData);
+    this.callToClearData();
+    this.doCheck();
+  }
+
+  callToClearData(){
+    this.name=null;
+    this.mailId=null;
+    this.number=null;
+    this.address=null;
+  }
+  doCheck(){
+    if(this.name != null && this.address !=null && this.number!=null && this.mailId !=null){
+      (document.getElementById('submitButton') as HTMLInputElement).disabled = false;
+    }
+    else{
+      (document.getElementById('submitButton') as HTMLInputElement).disabled = true;
+    }
+  }
 }
+
+
